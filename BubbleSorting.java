973101ea14b52bd3ba6e8bcdc17bcedfe0ca76c5
Sachith Nanayakkara <sachithmaths@gmail.com>
2019-07-29 11:47:38 +0530

    add exyra spaces

--- BubbleSorting.java
+++ BubbleSorting.java
@@ -1,13 +1,18 @@
 package sorting;
 
 public class BubbleSorting {
+    
     public static void main(String[] args) {
         int[] arr1 ={1,4,2,7,3,4};
         int n=arr1.length;
         int temp;
+        
         for(int i =0;i<n-1;i++){
+            
             for(int j=0; j<n-1-i;j++){
+                
                 if(arr1[j]>arr1[j+1]){
+                    
                     temp = arr1[j];
                     arr1[j]=arr1[j+1];
                     arr1[j+1]=temp;
@@ -15,6 +20,7 @@ public class BubbleSorting {
             }
 
         }
+        
         for (int i:arr1
              ) {
             System.out.print(i+", ");
