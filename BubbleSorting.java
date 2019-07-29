125ab5d5d7a749d9537e3b2286364b9de40dd435
Sachith Nanayakkara <sachithmaths@gmail.com>
2019-07-29 11:46:28 +0530

    Add bubble sorting

---
+++ BubbleSorting.java
@@ -0,0 +1,24 @@
+package sorting;
+
+public class BubbleSorting {
+    public static void main(String[] args) {
+        int[] arr1 ={1,4,2,7,3,4};
+        int n=arr1.length;
+        int temp;
+        for(int i =0;i<n-1;i++){
+            for(int j=0; j<n-1-i;j++){
+                if(arr1[j]>arr1[j+1]){
+                    temp = arr1[j];
+                    arr1[j]=arr1[j+1];
+                    arr1[j+1]=temp;
+                }
+            }
+
+        }
+        for (int i:arr1
+             ) {
+            System.out.print(i+", ");
+        }
+    }
+
+}
