72b972b81edba1994cb8aa4d5f6fe06e6c55bbb8
Sachith Nanayakkara <sachithmaths@gmail.com>
2019-07-30 13:07:28 +0530

    update SWAP

--- Swap.java
+++ Swap.java
@@ -1,30 +1,13 @@
 package algorithms;
+    public class Swap {
 
-import java.util.Scanner;
+        public static void main(String[] args) {
+            int num1=17,num2=62;
+            System.out.println("before swap\t num1: "+num1+"\tnum2: "+num2);
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            System.out.println("After swap\t num1: "+num1+"\tnum2: "+num2);
+        }
 
-public class Swap {
-
-	public static void main(String[] args) {
-
-		System.out.print("Please enter the first number : ");
-		Scanner sc = new Scanner(System.in);
-		Integer num1 = sc.nextInt();
-
-		System.out.print("Please enter the second number : ");
-		Scanner sc1 = new Scanner(System.in);
-		Integer num2 = sc1.nextInt();
-
-		System.out.println();
-		System.out.println("Before Swap --> ");
-		System.out.println("Frist Number : " + num1 + ", Second Number : " + num2);
-
-		num1 = num1 + num2;
-		num2 = num1 - num2;
-		num1 = num1 - num2;
-
-		System.out.println();
-		System.out.println("After Swap --> ");
-		System.out.println("Frist Number : " + num1 + ", Second Number : " + num2);
-	}
-
-}
+    }
