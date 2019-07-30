bc2c494e775904643f2c4acee64500907c4cef16
UdariChathurika <36497760+UdariChathurika@users.noreply.github.com>
2019-07-30 04:24:18 -0700

    Algo to find duplicate characters in a String

---
+++ FindDuplicateCharactersInAString.java
@@ -0,0 +1,34 @@
+package algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindDuplicateCharactersInAString {
+
+        public void findDuplicateChars(String str){
+
+            Map<Character, Integer> dupMap = new HashMap<Character, Integer>();
+            char[] chrs = str.toCharArray();
+            for(Character ch:chrs){
+                if(dupMap.containsKey(ch)){
+                    dupMap.put(ch, dupMap.get(ch)+1);
+                } else {
+                    dupMap.put(ch, 1);
+                }
+            }
+            Set<Character> keys = dupMap.keySet();
+            for(Character ch:keys){
+                if(dupMap.get(ch) > 1){
+                    System.out.println(ch+"--->"+dupMap.get(ch));
+                }
+            }
+        }
+
+        public static void main(String a[]){
+            FindDuplicateCharactersInAString fdcs = new FindDuplicateCharactersInAString();
+            fdcs.findDuplicateChars("java-basics");
+
+        }
+    }
+
